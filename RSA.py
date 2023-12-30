b2c8aa0c32745ed709488e06a4ae9bcc43749853
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2023-12-30 19:06:58 +0530

    Create RSA.py

---
+++ RSA.py
@@ -0,0 +1,61 @@
+#pip install cryptography
+
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization
+
+# Key generation
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+public_key = private_key.public_key()
+
+# Serialization of keys to PEM format
+private_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Save the keys to files
+with open('private_key.pem', 'wb') as f:
+    f.write(private_pem)
+
+with open('public_key.pem', 'wb') as f:
+    f.write(public_pem)
+
+# Encryption and Decryption
+message = b'This is a secret message.'
+
+# Encryption using public key
+ciphertext = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+# Decryption using private key
+plaintext = private_key.decrypt(
+    ciphertext,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+print(f"Original Message: {message.decode()}")
+print(f"Decrypted Message: {plaintext.decode()}")
